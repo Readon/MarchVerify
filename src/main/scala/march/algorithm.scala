3979b523ee7f753a32152e7989e2e8fc43c19ed3
Xiao Yindong <xydarcher@uestc.edu.cn>
2023-03-06 00:01:13 +0800

    增加存储器访问模块。

--- src/main/scala/march/algorithm.scala
+++ src/main/scala/march/algorithm.scala
@@ -13,22 +13,29 @@ case class Check[T <: Instruction](input: Stream[Instruction], data: Bool)
     extends Area {
   val writeStream = cloneOf(input)
   val readStream = cloneOf(input)
-  val hasFault = Vec(Reg(Bool) init (False), log2Up(input.addr.getBitsWidth))
+  val hasFault = Vec(Reg(Bool) init (False), 1 << input.addr.getBitsWidth)
 
   Vec(writeStream, readStream) <> StreamDemux[Instruction](
     input,
     input.isRead.asUInt,
     2
   )
-  writeStream.throwWhen(True)
-
   val checkStream = cloneOf(readStream)
+  val checking = checkStream.fire
+  writeStream.haltWhen(checking).throwWhen(True)
+
   checkStream <-< readStream
   checkStream.throwWhen(True)
-  val checking = checkStream.fire
 
   when(checking & data =/= checkStream.value) {
     hasFault(checkStream.addr) := True
   }
+}
+
+case class Access(input: Stream[Instruction]) extends Area {
+  val addrWidth = input.addr.getBitsWidth
+  val ram = Mem(Bool, 1 << addrWidth)
+  val data =
+    ram.readWriteSync(input.addr, input.value, input.fire, !input.isRead)
 }
 
