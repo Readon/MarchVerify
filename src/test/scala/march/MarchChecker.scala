fa79828d9c1b63fed9f480e2eaf4e93f844d4509
Yindong Xiao <xydarcher@qq.com>
2023-03-13 23:48:40 +0800

    增加March检查器。

---
+++ src/test/scala/march/MarchChecker.scala
@@ -0,0 +1,42 @@
+package uestsg.march
+
+import spinal.core._
+import spinal.core.formal._
+import spinal.lib.formal._
+
+class MarchChecker extends SpinalFormalFunSuite {
+  val ops = Array[String](
+    "00",
+    "10",
+    "01",
+    "11",
+    "00",
+    "10",
+    "01",
+    "11",
+    "00",
+    "10"
+  )
+  val elements = Seq(
+    (0, true),
+    (1, true),
+    (1, true),
+    (1, false),
+    (1, false),
+    (0, false),
+    (ops.length, false)
+  )
+
+  test("withNoFault") {
+    FormalConfig
+      .withBMC()
+      .doVerify(new Component {
+
+        val dut = FormalDut(new March(elements, ops, 3))
+        val reset = ClockDomain.current.isResetActive
+        assumeInitial(reset)
+
+        assert(dut.io.faults === 0)
+      })
+  }
+}
